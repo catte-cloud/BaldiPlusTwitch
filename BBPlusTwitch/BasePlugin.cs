d7f23c554c2a04db480e7d9cc15f8c18085a8abb
benjaminpants <maltbyfamily@gmail.com>
2021-06-22 16:46:46 -0700

    bepinex plugin stuff set up lol

---
+++ BBPlusTwitch/BasePlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Net;
+//BepInEx stuff
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using HarmonyLib; //god im hoping i got the right version of harmony
+//this code is reused from BaldiMP, lol
+
+namespace BaldiTwitch
+{
+    [BepInPlugin("mtm101.rulerp.bbplus.balditwitch", "BB+ Twitch Intergration", "0.0.0.0")]
+    public class BaldiTwitch : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Logger.LogDebug("hey wassup bb+ twitch mod here to say snas");
+            Harmony harmony = new Harmony("mtm101.rulerp.bbplus.balditwitch");
+
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,18 @@
+
+You'll need to get the following DLLS from BepInEx and your BB+ installation and put them in a folder called "Dependecies":
+
+0Harmony.dll
+Assembly-CSharp.dll
+BepInEx.dll
+BepInEx.Harmony.dll
+Rewired_Core.dll
+Rewired_Windows.dll
+Unity.TextMeshPro.dll
+UnityEngine.CoreModule.dll
+UnityEngine.dll
+UnityEngine.InputLegacyModule.dll
+UnityEngine.UI.dll
+UnityEngine.PhysicsModule.dll
+UnityEngine.AudioModule.dll
+UnityEngine.RenderPipelines.Core.Runtime.dll
+Unity.RenderPipelines.Universal.Runtime.dll
